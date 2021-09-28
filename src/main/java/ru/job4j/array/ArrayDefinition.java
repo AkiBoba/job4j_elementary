8455f5b3f364ee8dad87f59a07f3950a00ba279d
Vladimir Likhachev <lettervl@ya.ru>
2021-09-28 22:24:17 +0300

    6.0.1. Объявление массива. [#3613 #209208]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,10 +2,8 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        String[] names = new  String[4];
-        names[0] = "Petr Arsentev";
-        names[1] = "Ivan Ivan";
-        names[2] = "Alex Alex";
-        names[3] = "Serg Serg";
+        short[] ages = new short[10];
+        String[] surnames = new  String[100500];
+        float[] prices = new float[40];
     }
 }
