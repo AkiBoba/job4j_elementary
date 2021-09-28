a500aa10dc0f41ed1ba189889acfad2060659555
Vladimir Likhachev <lettervl@ya.ru>
2021-09-28 15:31:25 +0300

    6.0.1. Объявление массива. [#3613 #209208]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,9 +7,5 @@ public class ArrayDefinition {
         names[1] = "Ivan Ivan";
         names[2] = "Alex Alex";
         names[3] = "Serg Serg";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
     }
 }
